a02d2d90621ffed43cc6b109c7d4ec36d042d288
mikceroese <776990@unizar.es>
2021-03-19 10:39:23 +0100

    Primer commit - Servlet de login

---
+++ Web/servlets_frontend/login.java
@@ -0,0 +1,62 @@
+package servlets_frontend;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import org.apache.commons.io.IOUtils;
+import org.json.*;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+		String pass = request.getParameter("pass");
+		URL url = new URL("http://rocketruckus.westeurope.azurecontainer.io:8080/LoginUser?user=" + user + "&pass=" + pass);
+		URLConnection con = url.openConnection();
+		InputStream in = con.getInputStream();
+		String encoding = con.getContentEncoding();
+		encoding = encoding == null ? "UTF-8" : encoding;
+		String jotason = IOUtils.toString(in, encoding);
+		JSONObject obj = new JSONObject(jotason);
+		boolean result = obj.getBoolean("result");
+		if (result)
+		{
+			response.sendRedirect("home.html");
+		} else 
+		{
+			request.setAttribute("error", "Datos erroneos");
+			request.getRequestDispatcher("login.jsp").forward(request,response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
